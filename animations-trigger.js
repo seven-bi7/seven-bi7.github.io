01238db3af819f0d1f1fb480fcc5c976796fb764
EugenioCos <eugenio.costagliola@icloud.com>
2025-04-08 11:11:04 +0200

    site 3 homepage draft

--- animations-trigger.js
+++ animations-trigger.js
@@ -1,12 +1,14 @@
+
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            entry.target.getAnimations().forEach(animation => {
-                animation.play();
-            });
+            entry.target.classList.add(entry.target.getAttribute('data-animation'));
         }
     });
 });
 
 const animable = document.querySelectorAll('.animate');
-animable.forEach((element) => observer.observe(element));
+animable.forEach((element) => {
+    element.style.opacity = 0;
+    observer.observe(element);
+});
